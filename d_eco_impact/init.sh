5e04a659c10b708536c8f77be1a713da94042197
Hidde Elzinga <Hidde.Elzinga@deltares.nl>
2024-01-10 15:24:22 +0100

    Add first setup for d-ecoimpact service

---
+++ d_eco_impact/init.sh
@@ -0,0 +1,7 @@
+
+# pip install d-ecoimpact
+pip install git+https://github.com/Deltares/D-ecoimpact
+pip install s3fs
+
+# get run script
+wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/main.py
