955ef99e2d41de61fb91ad063e935ec6250cb304
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-01-10 12:17:19 +0100

    added modelbuilder init and upload scripts

---
+++ modelbuilder/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip install dfm_tools
+pip install git+https://github.com/deltares/dfm_tools
+wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/modelbuilder_example.ipynb .
+#wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/postprocessing_example.ipynb .
---
+++ modelbuilder/upload_model_zip.py
@@ -0,0 +1,21 @@
+import os
+import zipfile
+import s3fs
+
+# zip model input files
+dir_model = "Vietnam_model"
+file_zip = f"{dir_model}.zip"
+with zipfile.ZipFile(file_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    for dirname, subdirs, files in os.walk(dir_model):
+        if "/data" in dirname:
+            continue
+        zipf.write(dirname)
+        for filename in files:
+            zipf.write(os.path.join(dirname, filename))
+
+# upload zip to s3 bucket
+S3_ENDPOINT_URL = os.environ["S3_ENDPOINT"]
+fs = s3fs.S3FileSystem(client_kwargs={'endpoint_url': S3_ENDPOINT_URL})
+onxia_user_name = os.environ["GIT_USER_NAME"] #TODO: convenient to have the username in a separate env var
+bucket_name = f"oidc-{onxia_user_name}"
+fs.upload(file_zip, f'{bucket_name}/{file_zip}')
