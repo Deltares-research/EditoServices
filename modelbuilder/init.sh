2a63ffb6827cca806813c370dbadbef09103f487
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-01-11 21:41:57 +0100

    upload folder instead of zip

--- modelbuilder/init.sh
+++ modelbuilder/init.sh
@@ -4,4 +4,4 @@ pip install dfm_tools
 pip install git+https://github.com/deltares/dfm_tools
 wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/modelbuilder_example.ipynb
 #wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/postprocessing_example.ipynb
-wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/modelbuilder/upload_model_zip.py
+wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/modelbuilder/upload_model.py
