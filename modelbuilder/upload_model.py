c83649a479c8d98ab4c6247bbad657484b17968b
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-01-11 22:01:32 +0100

    prevent nested model_dir on s3

--- modelbuilder/upload_model.py
+++ modelbuilder/upload_model.py
@@ -14,12 +14,22 @@ dir_model = "Vietnam_model"
 #        for filename in files:
 #            zipf.write(os.path.join(dirname, filename))
 
-shutil.rmtree(os.path.join(dir_model, "data"))
+# clean dir_model before upload
+dir_data = os.path.join(dir_model, "data")
+if os.path.exists(dir_data):
+    shutil.rmtree(dir_data)
 
-# upload zip to s3 bucket
+# setup s3 bucket
 S3_ENDPOINT_URL = os.environ["S3_ENDPOINT"]
 fs = s3fs.S3FileSystem(client_kwargs={'endpoint_url': S3_ENDPOINT_URL})
 onxia_user_name = os.environ["GIT_USER_NAME"] #TODO: convenient to have the username in a separate env var
 bucket_name = f"oidc-{onxia_user_name}"
+
+# remove dir_model_s3 folder if present, to prevent nested dir_model in dir_model
+dir_model_s3 = f'{bucket_name}/{dir_model}'
+if fs.exists(dir_model_s3):
+    print(f"overwriting existing '{dir_model}' folder on s3")
+    fs.rm(dir_model_s3, recursive=True)
+
 #fs.upload(file_zip, f'{bucket_name}/{file_zip}')
 fs.upload(dir_model, f'{bucket_name}/{dir_model}', recursive=True)
