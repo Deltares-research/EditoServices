69c9dd89aaa4590856f6ebd1012c3727c8064056
Mostafa Farrag <moah.farag@gmail.com>
2024-02-16 13:59:08 +0100

    update example

--- d_eco_container/model/check-run-status.sh
+++ d_eco_container/model/check-run-status.sh
@@ -2,4 +2,4 @@
 
 URL=http://localhost:5001
 
-curl $URL/check-run-status/a33fa5fa-5ef3-4e90-a6ab-343b90b3ea87
+curl $URL/check-run-status/<operation_id>
--- d_eco_container/model/download-results.sh
+++ d_eco_container/model/download-results.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -OJ http://localhost:5001/get-result/86cc2467-5de2-4fa4-8310-91c7285f96c5
+URL=http://localhost:5001
+curl -OJ $URL/get-result/<operation_id>
