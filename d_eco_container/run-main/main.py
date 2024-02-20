f8feea17ec6e148f521e8a73be7b668db3b55254
Mostafa Farrag <moah.farag@gmail.com>
2024-02-20 13:52:05 +0100

    container to run main,py

---
+++ d_eco_container/run-main/main.py
@@ -0,0 +1,42 @@
+import os
+import s3fs
+from pathlib import Path
+
+from decoimpact.business.application import Application
+from decoimpact.business.workflow.model_builder import ModelBuilder
+from decoimpact.crosscutting.i_logger import ILogger
+from decoimpact.crosscutting.logger_factory import LoggerFactory
+from decoimpact.data.entities.data_access_layer import DataAccessLayer, IDataAccessLayer
+
+# Create an file-system object for the s3 bucket
+onxia_user_name = os.environ["GIT_USER_NAME"]
+bucket_name = f"oidc-{onxia_user_name}"
+S3_ENDPOINT_URL = os.environ["S3_ENDPOINT"]
+fs = s3fs.S3FileSystem(client_kwargs={'endpoint_url': S3_ENDPOINT_URL})
+
+yaml_file = os.environ["yaml-file-path"]
+yaml_file_path = Path(yaml_file)
+
+
+# download the yaml file
+fs.download(f"{bucket_name}/{yaml_file}", yaml_file)
+
+# configure logger and data-access layer for d-ecoimpact
+logger: ILogger = LoggerFactory.create_logger()
+da_layer: IDataAccessLayer = DataAccessLayer(logger)
+model_builder = ModelBuilder(da_layer, logger)
+
+# read the input (nc) and output (nc) path from the yaml input file
+model_data = da_layer.read_input_file(yaml_file_path)
+nc_file = str(model_data.datasets[0].path.relative_to(os.getcwd()))
+output_file_path = str(model_data.output_path)
+
+# download input netCDF (UGrid) file
+fs.download(f"{bucket_name}/{nc_file}", nc_file)
+
+# create and run d-ecoimpact application
+application = Application(logger, da_layer, model_builder)
+application.run(yaml_file_path)
+
+# upload the output file to s3
+fs.upload(output_file_path, f"{bucket_name}/{output_file_path}")
