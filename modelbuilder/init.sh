5394b0fac2d89b9aa60a3df834abfb698c19d53a
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2024-06-03 17:50:34 +0200

    removed git install and removed duplicated files

--- modelbuilder/init.sh
+++ modelbuilder/init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 pip install dfm_tools
-pip install git+https://github.com/deltares/dfm_tools
+# pip install git+https://github.com/deltares/dfm_tools
 wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/modelbuilder_example.ipynb
 #wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/postprocessing_example.ipynb
 wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/modelbuilder/upload_model.py
+
+# somehow the files get downloaded twice, so delete *.1 files
+rm -f *.1
