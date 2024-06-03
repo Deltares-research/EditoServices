f6b171aacacbf23440536f5738bcdb9255504df8
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2024-06-03 18:21:25 +0200

    install dfmt via gitlink again

--- modelbuilder/init.sh
+++ modelbuilder/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pip install dfm_tools
-# pip install git+https://github.com/deltares/dfm_tools
+pip install git+https://github.com/deltares/dfm_tools
 wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/modelbuilder_example.ipynb
 #wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/postprocessing_example.ipynb
 wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/modelbuilder/upload_model.py
