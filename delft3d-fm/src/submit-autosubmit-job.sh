4e32433f907d1c494fdd2f31655721720b1370e5
Mostafa Farrag <moah.farag@gmail.com>
2024-09-10 17:47:57 +0200

    replace the hard coded dimr run to be more general

--- delft3d-fm/src/submit-autosubmit-job.sh
+++ delft3d-fm/src/submit-autosubmit-job.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
 model_dir="/u/farrag/containers/test-case/original"
+# The name of the DIMR configuration file. The default name is dimr_config.xml. This file must already exist!
+executable=dimr
+executable_opts=dimr_config.xml
+
+SLURM_NTASKS=1
+
 # Setup the model
 # Specify the ROOT folder of your model, i.e. the folder that contains ALL of the input files and sub-folders, e.g:
 # model_dir/
@@ -29,25 +35,23 @@ mdu_file_dir=$model_dir/dflowfm
 # Specify the folder containing your DIMR configuration file.
 dimr_config_dir=$model_dir
 
-# The name of the DIMR configuration file. The default name is dimr_config.xml. This file must already exist!
-dimr_file=dimr_config.xml
-
 # This setting might help to prevent errors due to temporary locking of NetCDF files.
 export HDF5_USE_FILE_LOCKING=FALSE
-
 
 # Stop the computation after an error occurs.
 set -e
 
-# For parallel processes, the lines below update the <process> element in the DIMR configuration file.
-# The updated list of numbered partitions is calculated from the user specified number of nodes and cores.
-# You DO NOT need to modify the lines below.
-PROCESSSTR="$(seq -s " " 0 $((SLURM_NTASKS-1)))"
-sed -i "s/\(<process.*>\)[^<>]*\(<\/process.*\)/\1$PROCESSSTR\2/" $dimr_config_dir/$dimr_file
+if [ "$executable" = "dimr" ]; then
+  # For parallel processes, the lines below update the <process> element in the DIMR configuration file.
+  # The updated list of numbered partitions is calculated from the user specified number of nodes and cores.
+  # You DO NOT need to modify the lines below.
+  PROCESSSTR="$(seq -s " " 0 $((SLURM_NTASKS-1)))"
+  sed -i "s/\(<process.*>\)[^<>]*\(<\/process.*\)/\1$PROCESSSTR\2/" "$dimr_config_dir"/"$executable_opts"
 
-# The name of the MDU file is read from the DIMR configuration file.
-# You DO NOT need to modify the line below.
-mdu_file="$(sed -n 's/\r//; s/<inputFile>\(.*\).mdu<\/inputFile>/\1/p' $dimr_config_dir/$dimr_file)".mdu
+  # The name of the MDU file is read from the DIMR configuration file.
+  # You DO NOT need to modify the line below.
+  mdu_file="$(sed -n 's/\r//; s/<inputFile>\(.*\).mdu<\/inputFile>/\1/p' "$dimr_config_dir"/"$executable_opts")".mdu
+fi
 
 
 #--- Partition by calling the dflowfm executable -------------------------------------------------------------
@@ -68,6 +72,6 @@ echo ""
 echo "Simulation..."
 cd "$dimr_config_dir"
 
-echo "Running DIMR in dir ${PWD}, with config file $dimr_file"
-$SCRIPT_PATH -c "$CONTAINER_PATH" -m "$model_dir" dimr "$dimr_file"
+echo "Running $executable in dir ${PWD}, with config file $executable_opts"
+$SCRIPT_PATH -c "$CONTAINER_PATH" -m "$model_dir" "$executable" "$executable_opts"
 echo "Submitted job: $SLURM_JOB_ID Number of partitions: $SLURM_NTASKS"
