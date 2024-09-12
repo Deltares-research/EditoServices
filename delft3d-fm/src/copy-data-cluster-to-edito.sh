ee898d939e3c126f5be7b99be8122580aed05346
Mostafa Farrag <moah.farag@gmail.com>
2024-09-12 13:55:01 +0200

    optimize the copy results script

--- delft3d-fm/src/copy-data-cluster-to-edito.sh
+++ delft3d-fm/src/copy-data-cluster-to-edito.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 echo "Current working directory: ${PWD}"
 USER=delt550999
-scp -r $USER@glogin1.bsc.es:from-edito/dflowfm/ results/
+cd "/home/delt/$USER/from-edito" || echo "Error: could not change to the directory /home/delt/$USER/from-edito" && exit 1
+scp -r $USER@glogin1.bsc.es:dflowfm/ results/
