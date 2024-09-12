15b20872e1de36ef1ac6b20c82ef8fd5c9ec05f8
Mostafa Farrag <moah.farag@gmail.com>
2024-09-12 14:02:13 +0200

    remove the || and && conditions

--- delft3d-fm/src/copy-data-cluster-to-edito.sh
+++ delft3d-fm/src/copy-data-cluster-to-edito.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 echo "Current working directory: ${PWD}"
 USER=delt550999
-cd "/home/delt/$USER/from-edito" || echo "Error: could not change to the directory /home/delt/$USER/from-edito" && exit 1
+cd "/home/delt/$USER/from-edito"
 scp -r $USER@glogin1.bsc.es:dflowfm/ results/
