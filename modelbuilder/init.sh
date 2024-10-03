2a090229b09e5481682a5cf1c33e324a3f84ce5e
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2024-10-03 13:56:27 +0200

    comment installation from main to save time

--- modelbuilder/init.sh
+++ modelbuilder/init.sh
@@ -5,14 +5,16 @@ set -e
 
 # we require dfm_tools>=0.29.0 since it works with new CDS
 pip install "dfm_tools>=0.29.0"
-pip install git+https://github.com/deltares/dfm_tools
+# pip install git+https://github.com/deltares/dfm_tools
 wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/modelbuilder_example.ipynb
 #wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/postprocessing_example.ipynb
 wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/modelbuilder/upload_model.py
 
-# extend modelbuilder notebook
-wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/modelbuilder/extend_modelbuilder_notebook.py
+# clear output
 jupyter nbconvert --clear-output --inplace modelbuilder_example.ipynb
+# extend modelbuilder notebook, creates modelbuilder_example_edito.ipynb
+wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/modelbuilder/extend_modelbuilder_notebook.py
 python extend_modelbuilder_notebook.py
+# remove redundant files
 rm modelbuilder_example.ipynb
 rm extend_modelbuilder_notebook.py
