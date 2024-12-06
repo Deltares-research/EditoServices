9586ed30a121fa6b413d3875f9f5ba2b87de7df5
Felix-Deltares <131261890+Felix-Deltares@users.noreply.github.com>
2024-12-06 11:05:54 +0100

    Reorganize init script

--- dfm-postprocess/init.sh
+++ dfm-postprocess/init.sh
@@ -6,8 +6,10 @@ set -e
 # we require dfm_tools>=0.29.0 since it works with new CDS
 pip install "dfm_tools>=0.29.0"
 # pip install git+https://github.com/deltares/dfm_tools
-# wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/modelbuilder_example.ipynb
+
+# open notebooks
 wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/postprocessing_example.ipynb
+# wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/modelbuilder_example.ipynb
 # wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/modelbuilder/upload_model.py
 
 # clear output
