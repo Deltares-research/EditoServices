9c83f27082e192c63ed27e1a03cae3ea96b9e15b
Felix-Deltares <131261890+Felix-Deltares@users.noreply.github.com>
2024-12-09 10:39:32 +0100

    Update init.sh with raw validation data

--- d_eco_impact_postprocess/init.sh
+++ d_eco_impact_postprocess/init.sh
@@ -7,13 +7,15 @@ set -e
 pip install "dfm_tools>=0.29.0"
 # pip install git+https://github.com/deltares/dfm_tools
 
-# open notebooks
-# wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/postprocessing_example.ipynb
+# open notebook
 wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/d_eco_impact_postprocess/postprocessing_example.ipynb
-wget '-p' https://raw.githubusercontent.com/Deltares-research/EditoServices/main/d_eco_impact_postprocess/validation-data/ 
 
-# wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/modelbuilder_example.ipynb
-# wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/modelbuilder/upload_model.py
+# open validation data
+wget https://github.com/Felix-Deltares/EditoServices/raw/0d19ccdb5733f76fcb663c7cc5297135ce2a09e6/d_eco_impact_postprocess/validation-data/zostera_noltei_2017Polygon_WGS84.shp
+#wget '-p' https://raw.githubusercontent.com/Deltares-research/EditoServices/main/d_eco_impact_postprocess/validation-data/ 
+
+
+
 
 # clear output
 jupyter nbconvert --clear-output --inplace postprocessing_example.ipynb
