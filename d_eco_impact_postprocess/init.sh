a4e33b6d10ce0b6535cb590ba31aa3542cbe06ec
Felix-Deltares <131261890+Felix-Deltares@users.noreply.github.com>
2024-12-09 10:53:21 +0100

    try going up 4 folders in saving validation file

--- d_eco_impact_postprocess/init.sh
+++ d_eco_impact_postprocess/init.sh
@@ -11,7 +11,7 @@ pip install "dfm_tools>=0.29.0"
 wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/d_eco_impact_postprocess/postprocessing_example.ipynb
 
 # open validation data
-wget -p /validation-data https://github.com/Felix-Deltares/EditoServices/raw/0d19ccdb5733f76fcb663c7cc5297135ce2a09e6/d_eco_impact_postprocess/validation-data/zostera_noltei_2017Polygon_WGS84.shp
+wget -p ../../../../validation-data https://github.com/Felix-Deltares/EditoServices/raw/0d19ccdb5733f76fcb663c7cc5297135ce2a09e6/d_eco_impact_postprocess/validation-data/zostera_noltei_2017Polygon_WGS84.shp
 #wget '-p' https://raw.githubusercontent.com/Deltares-research/EditoServices/main/d_eco_impact_postprocess/validation-data/ 
 
 
