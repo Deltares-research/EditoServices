173fa89d7041177df40be2fbcd3447ef5d33c539
Felix-Deltares <131261890+Felix-Deltares@users.noreply.github.com>
2025-05-23 11:50:45 +0200

    Update upload_plots.py
    debugged EDITO service

--- d_eco_impact_postprocess/upload_plots.py
+++ d_eco_impact_postprocess/upload_plots.py
@@ -13,14 +13,14 @@ def upload_plots_to_s3_bucket(dir_out):
     fs = s3fs.S3FileSystem(client_kwargs={'endpoint_url': S3_ENDPOINT_URL})
     onxia_user_name = os.environ["GIT_USER_NAME"] #TODO: convenient to have the username in a separate env var
     bucket_name = f"oidc-{onxia_user_name}"
-
-    # remove dir_out_s3 folder if present, to prevent nested dir_out in dir_out
-    dir_out_s3 = f'{bucket_name}/{out}' #alternative option is {os.path.basename(dir_out)}
+    
+        # remove dir_out_s3 folder if present, to prevent nested dir_out in dir_out
+    dir_out_s3 = f'{bucket_name}/HSI_plots' #alternative option is {os.path.basename(dir_model)}
     if fs.exists(dir_out_s3):
         print(f"removing existing folder '{dir_out_s3}' on s3")
         fs.rm(dir_out_s3, recursive=True)
-    
-    # upload all files in model directory
+        
+        # upload all files in model directory
     list_files = [x for x in os.listdir(dir_out) if os.path.isfile(os.path.join(dir_out,x))]
     list_files.sort()
     print(f"uploading files from '{dir_out}' to '{dir_out_s3}':")
