7ce49cebc918c6bb3cf51ca226e7be29334f1298
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2025-06-11 12:04:49 +0200

    Update init.sh

--- modelbuilder/init.sh
+++ modelbuilder/init.sh
@@ -5,7 +5,8 @@ set -e
 
 # we require dfm_tools>=0.29.0 since it works with new CDS
 pip install "dfm_tools>=0.29.0"
-# pip install git+https://github.com/deltares/dfm_tools
+# required to include https://github.com/Deltares/dfm_tools/issues/1213
+pip install git+https://github.com/deltares/dfm_tools
 wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/modelbuilder_example.ipynb
 #wget https://github.com/Deltares/dfm_tools/raw/main/docs/notebooks/postprocessing_example.ipynb
 wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/modelbuilder/upload_model.py
