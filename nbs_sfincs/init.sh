fbea3f0f7608f790b0647a362c0fa71bf9cbbf0e
lorincmeszaros <lorinc.meszaros@deltares.nl>
2025-07-28 14:34:58 +0200

    create nbs_sfincs

---
+++ nbs_sfincs/init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# kill process if anything fails
+set -e
+
+# we require dfm_tools>=0.29.0 since it works with new CDS
+pip install "dfm_tools>=0.29.0"
+# pip install git+https://github.com/deltares/dfm_tools
+
+# open notebook
+wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/d_eco_impact_postprocess/postprocessing_example.ipynb
+wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/d_eco_impact_postprocess/upload_plots.py
+
+# open validation data
+#wget wget -p --convert-links -nH -nd -Pvalidation-data https://github.com/Felix-Deltares/EditoServices/raw/0d19ccdb5733f76fcb663c7cc5297135ce2a09e6/d_eco_impact_postprocess/validation-data/zostera_noltei_2017Polygon_WGS84.shp
+wget -p --convert-links -nH -nd -Pvalidation-data https://github.com/Deltares-research/EditoServices/raw/refs/heads/main/d_eco_impact_postprocess/validation-data/zostera_noltei_2017Polygon_WGS84.cpg
+wget -p --convert-links -nH -nd -Pvalidation-data https://github.com/Deltares-research/EditoServices/raw/refs/heads/main/d_eco_impact_postprocess/validation-data/zostera_noltei_2017Polygon_WGS84.dbf
+wget -p --convert-links -nH -nd -Pvalidation-data https://github.com/Deltares-research/EditoServices/raw/refs/heads/main/d_eco_impact_postprocess/validation-data/zostera_noltei_2017Polygon_WGS84.prj
+wget -p --convert-links -nH -nd -Pvalidation-data https://github.com/Deltares-research/EditoServices/raw/refs/heads/main/d_eco_impact_postprocess/validation-data/zostera_noltei_2017Polygon_WGS84.qmd
+wget -p --convert-links -nH -nd -Pvalidation-data https://github.com/Deltares-research/EditoServices/raw/refs/heads/main/d_eco_impact_postprocess/validation-data/zostera_noltei_2017Polygon_WGS84.shp
+wget -p --convert-links -nH -nd -Pvalidation-data https://github.com/Deltares-research/EditoServices/raw/refs/heads/main/d_eco_impact_postprocess/validation-data/zostera_noltei_2017Polygon_WGS84.shx
+
+
+# clear output
+jupyter nbconvert --clear-output --inplace postprocessing_example.ipynb
+
+# extend modelbuilder notebook, creates modelbuilder_example_edito.ipynb
+# wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/modelbuilder/extend_modelbuilder_notebook.py
+# python extend_modelbuilder_notebook.py
+# remove redundant files
+# rm modelbuilder_example.ipynb
+# rm extend_modelbuilder_notebook.py
---
+++ nbs_sfincs/upload_model.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+import s3fs
+
+def upload_model_to_s3_bucket(dir_model):
+    # directory with model input files
+    if not os.path.exists(dir_model):
+        raise FileNotFoundError(f"the folder {dir_model} does not exists, supply a different one")
+    assert os.path.isdir(dir_model)
+
+    # setup s3 bucket
+    S3_ENDPOINT_URL = os.environ["S3_ENDPOINT"]
+    fs = s3fs.S3FileSystem(client_kwargs={'endpoint_url': S3_ENDPOINT_URL})
+    onxia_user_name = os.environ["GIT_USER_NAME"] #TODO: convenient to have the username in a separate env var
+    bucket_name = f"oidc-{onxia_user_name}"
+
+    # remove dir_model_s3 folder if present, to prevent nested dir_model in dir_model
+    dir_model_s3 = f'{bucket_name}/DFM_INPUT' #alternative option is {os.path.basename(dir_model)}
+    if fs.exists(dir_model_s3):
+        print(f"removing existing folder '{dir_model_s3}' on s3")
+        fs.rm(dir_model_s3, recursive=True)
+    
+    # upload all files in model directory
+    list_files = [x for x in os.listdir(dir_model) if os.path.isfile(os.path.join(dir_model,x))]
+    list_files.sort()
+    print(f"uploading files from '{dir_model}' to '{dir_model_s3}':")
+    for model_file in list_files:
+        model_file_full = os.path.join(dir_model, model_file)
+        print(f" - {model_file}")
+        fs.upload(model_file_full, os.path.join(dir_model_s3,model_file))
+    print("upload finished.")
